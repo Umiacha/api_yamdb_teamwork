acecd12feec2fe76f61a2770d17a3faccc2352f8
Sulaev Vladimir <umiacha@yandex.ru>
2023-10-10 22:30:02 +0300

    Add schema for urls

--- api_yamdb/users/urls.py
+++ api_yamdb/users/urls.py
@@ -1,1 +1,7 @@
 from django.urls import path
+
+
+urlpatterns = [
+    path('auth/signup/', ...),  # регистрируем уже лежащего в БД пользователя
+    path('auth/token/', ...),  # даем зарегистрированному пользователю jwt-токен
+]
