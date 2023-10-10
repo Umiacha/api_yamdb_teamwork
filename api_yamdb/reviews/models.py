9e0f0cd0b36c5bb275579ce64a72da7a14dae2d6
Даниил Пастунов <dflient@icloud.com>
2023-10-10 22:33:04 +0300

    add models and views for title

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,55 @@
 from django.db import models
+from django.core.validators import MaxValueValidator
+from django.utils import timezone
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField('Название', max_length=256)
+    slug = models.SlugField(unique=True)
+
+
+class Genre(models.Model):
+    name = models.CharField('Название', max_length=256)
+    slug = models.SlugField(unique=True)
+
+
+class Title(models.Model):
+    name = models.CharField('Название', max_length=256)
+    year = models.IntegerField(
+        'Год выпуска',
+        validators=[MaxValueValidator(timezone.now().year)],
+    )
+    rating = models.IntegerField(
+        'Рейтинг произведения', default=0
+    )
+    description  = models.TextField(
+        'Описание',
+        null=True, blank=True
+    )
+    genre = models.ManyToManyField(
+        Genre, verbose_name='Slug жанра',
+        through='GenreTitle'
+    )
+    category = models.ForeignKey(
+        Category, verbose_name='Slug категории',
+        null=True, on_delete=models.SET_NULL,
+        related_name='titles'
+    )
+
+    class Meta:
+        verbose_name = 'Произведение'
+        verbose_name_plural = 'Произведения'
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class GenreTitle(models.Model):
+    title = models.ForeignKey(
+        Title, on_delete=models.SET_NULL,
+        null=True
+    )
+    genre = models.ForeignKey(
+        Genre, on_delete=models.SET_NULL,
+        null=True
+    )
