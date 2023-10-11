b22ee4f7702e6c211665ab2bd466f2c430455fc2
Sulaev Vladimir <umiacha@yandex.ru>
2023-10-11 05:02:11 +0300

    Move USERS_ROLES to models

--- api_yamdb/users/models.py
+++ api_yamdb/users/models.py
@@ -21,7 +21,12 @@ def check_name(value):
 
 
 class CustomUser(AbstractUser):
+    USERS_ROLES = [
+        ('user', 'user'),
+        ('moderator', 'moderator'),
+        ('admin', 'admin'),
+    ]
     username = models.CharField('Никнейм', max_length=150, unique=True, validators=[check_name])
     email = models.CharField('Почта', max_length=150, unique=True)
     bio = models.TextField('Биография', blank=True)
-    role = models.CharField('Роль', max_length=150, default='user')
+    role = models.CharField('Роль', max_length=150, default='user', choices=USERS_ROLES)
--- api_yamdb/users/serializers.py
+++ api_yamdb/users/serializers.py
@@ -2,8 +2,6 @@ from django.contrib.auth import get_user_model
 from rest_framework import serializers
 
 
-USERS_ROLES = ['user', 'moderator', 'admin',]
-
 User = get_user_model()
 
 
