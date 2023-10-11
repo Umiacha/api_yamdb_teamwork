17e242ae41b67bbd6e6cc21026d38e77847ada2a
Sulaev Vladimir <umiacha@yandex.ru>
2023-10-11 05:13:40 +0300

    Add some endpoints and start write viewsets

--- api_yamdb/users/urls.py
+++ api_yamdb/users/urls.py
@@ -1,7 +1,16 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import SimpleRouter
+
+from .views import AdminViewSet, UserViewSet
+
+
+users_router_v1 = SimpleRouter()
+users_router_v1.register('users', AdminViewSet)
 
 
 urlpatterns = [
     path('auth/signup/', ...),  # регистрируем уже лежащего в БД пользователя
     path('auth/token/', ...),  # даем зарегистрированному пользователю jwt-токен
+    path('users/me', UserViewSet.as_view()),
+    path('', include(users_router_v1.urls)),
 ]
--- api_yamdb/users/views.py
+++ api_yamdb/users/views.py
@@ -1,9 +1,14 @@
 from django.contrib.auth import get_user_model
-from rest_framework.viewsets import ModelViewSet
+from rest_framework.viewsets import ModelViewSet, GenericViewSet
+from rest_framework.mixins import RetrieveModelMixin, UpdateModelMixin
 
 
 User = get_user_model()
 
 
-class UserViewSet(ModelViewSet):
+class AdminViewSet(ModelViewSet):
     queryset = User.objects.all()
+
+
+class UserViewSet(RetrieveModelMixin, UpdateModelMixin, GenericViewSet):
+    lookup_field=None  # возможно, определю по-другому, но пользователь никакие аргументы не передает
