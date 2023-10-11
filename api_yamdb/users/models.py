84238b882153de73c48d1fc09f79efa7321bd1c4
Sulaev Vladimir <umiacha@yandex.ru>
2023-10-11 04:55:08 +0300

    Add validation to username in user model

--- api_yamdb/users/models.py
+++ api_yamdb/users/models.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import AbstractUser
+from django.core.exceptions import ValidationError
 from django.db import models
 
 
@@ -14,8 +15,13 @@ from django.db import models
 Пока что думаю, что ограничение на проект -- IsAuthenticatedOrReadonly (ограничения на анонимов).
 """
 
+def check_name(value):
+    if value == 'me':
+        raise ValidationError('Нельзя создать пользователя с никнеймом "me"!')
+
+
 class CustomUser(AbstractUser):
-    username = models.CharField('Никнейм', max_length=150, unique=True)
+    username = models.CharField('Никнейм', max_length=150, unique=True, validators=[check_name])
     email = models.CharField('Почта', max_length=150, unique=True)
     bio = models.TextField('Биография', blank=True)
     role = models.CharField('Роль', max_length=150, default='user')
