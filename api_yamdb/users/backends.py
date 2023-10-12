f2c269b9487cdbba3db6c21ebfc7e6a76ac0e2b5
Sulaev Vladimir <umiacha@yandex.ru>
2023-10-12 14:32:56 +0300

    Write jwt backend

--- api_yamdb/users/backends.py
+++ api_yamdb/users/backends.py
@@ -1,9 +1,55 @@
 import jwt
-from rest_framework.authentication import BaseAuthentication
+
+from django.contrib.auth import get_user_model
+
+from rest_framework.authentication import BaseAuthentication, get_authorization_header
+from rest_framework.exceptions import AuthenticationFailed
 
 
-# Задача: по статье (https://habr.com/ru/articles/538040/) создать динамический атрибут пользователя token
+# Задача: по статье (https://habr.com/ru/articles/538040/) создать динамический метод пользователя token
 # и создать здесь бэкенд для проверки jwt-токена.
 # Цель: проверить, учитывается ли время жизни токена ('exp') автоматически и скорректировать выдачу токена.
 # 
 # Задача: продумать, как в эту систему встроить confirmation_code (желательно, без сохранения в БД).
+
+User = get_user_model()
+
+
+class JWTAuthentification(BaseAuthentication):
+    AUTH_HEADER_PREFIX = 'Bearer'
+    
+    def authenticate(self, request):
+        request.user = None
+        auth_header = get_authorization_header(request).split()
+        
+        # Если заголовка нет или он некорректен, то пользователь считается анонимным.
+        if not auth_header or len(auth_header) == 1 or len(auth_header) > 2:
+            return None
+        
+        prefix = auth_header[0].decode('utf-8')
+        token = auth_header[1].decode('utf-8')
+        
+        # Если не тот префикс, которые мы ожидали.
+        if prefix != self.AUTH_HEADER_PREFIX:
+            return None
+        
+        try:
+            payload = jwt.decode(token, 'secret')  # Не, этот "secret" явно надо менять...
+        except Exception:
+            raise AuthenticationFailed(
+                'Отказ в доступе: невозможно декодировать токен!'
+            )
+        
+        try:
+            user = User.objects.get(pk=payload['uid'])
+        except User.DoesNotExist:
+            raise AuthenticationFailed(
+                'Пользователь, соответствующий токену не найден!'
+            )
+        
+        if not user.is_active:
+            raise AuthenticationFailed(
+                'Данный пользователь деактивирован!'
+            )
+        
+        return (user, token)
--- api_yamdb/users/views.py
+++ api_yamdb/users/views.py
@@ -21,14 +21,14 @@ def get_token(request):
     if not username:
         return Response(data={"username": "Отсутствует поле или оно некорректно!"}, status=status.HTTP_400_BAD_REQUEST)
     try:
-        User.objects.get(username=username)
+        user = User.objects.get(username=username)
     except User.DoesNotExist:
         raise NotFound('Пользователь не найден!')
     # Пока почта не настроена, confirmation_code всегда должен быть 1!
     if not confirmation_code or confirmation_code != 1:
         return Response(data={"confirmation_code": "Отсутствует поле или оно некорректно!"}, status=status.HTTP_400_BAD_REQUEST)
     user_token = jwt.encode(
-        payload={...},  # Тут 100% будет "exp" со значением из настроек, но... Что еще?
+        payload={'uid': user.id, ...},  # Тут 100% будет "exp" со значением из настроек, но... Что еще?
         key='secret',  # думаю, надо будет это как-то поменять!
         algorithm='HS256'
     )
