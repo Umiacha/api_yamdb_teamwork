adc2b7caf9069fd45d08bbe0d50bf4305f27dd4a
Даниил Пастунов <dflient@icloud.com>
2023-10-12 21:22:06 +0300

    test

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,13 +1,57 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, mixins, filters
 from django_filters.rest_framework import DjangoFilterBackend
 
-from reviews.models import Title
-from .serializers import TitleSerializer
+from reviews.models import Title, Genre, Category
+from .serializers import (
+    TitleSerializer, GenreSerializer, CategorySerializer
+)
 
-class TitleViewSet(viewsets.ModelViewSet):
+
+class PatchModelMixin(mixins.UpdateModelMixin):
+    """Mixin для обновления объекта с использованием метода PATCH."""
+
+    def partial_update(self, request, *args, **kwargs):
+        kwargs['partial'] = True
+        return self.update(request, *args, **kwargs)
+
+
+class TitleViewSet(
+    mixins.ListModelMixin,
+    mixins.CreateModelMixin,
+    mixins.RetrieveModelMixin,
+    mixins.DestroyModelMixin,
+    PatchModelMixin,
+    viewsets.GenericViewSet
+):
     queryset = Title.objects.all()
     serializer_class = TitleSerializer
     filter_backends = (DjangoFilterBackend,)
     filterset_fields = (
         'category', 'genre', 'name', 'year'
     )
+
+
+class GenreViewSet(
+    mixins.ListModelMixin,
+    mixins.CreateModelMixin,
+    mixins.DestroyModelMixin,
+    viewsets.GenericViewSet
+):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+    lookup_field = 'slug'
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name',)
+
+
+class CategoryViewSet(
+    mixins.ListModelMixin,
+    mixins.CreateModelMixin,
+    mixins.DestroyModelMixin,
+    viewsets.GenericViewSet
+):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    lookup_field = 'slug'
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name',)
