230ea5038b8bc3e34fc3b6c39faf5af9b434ff00
Михаил Валюшицкий <michaelkiller97@gmail.com>
2023-10-14 13:13:18 +0300

    models changing

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -50,7 +50,7 @@ class GenreTitle(models.Model):
 
 
 class Review(models.Model):
-    title = models.ForeignKey(
+    title_id = models.ForeignKey(
         Title,
         on_delete=models.CASCADE,
         related_name="reviews",
