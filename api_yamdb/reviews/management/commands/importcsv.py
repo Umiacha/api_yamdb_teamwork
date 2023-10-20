0ec4c523dc3302fba7804d8cfb556ff440125100
Михаил Валюшицкий <michaelkiller97@gmail.com>
2023-10-20 08:51:51 +0300

    changed csvimport

--- api_yamdb/reviews/management/commands/importcsv.py
+++ api_yamdb/reviews/management/commands/importcsv.py
@@ -1,114 +1,88 @@
 import csv
 import os
-
-from django.core.management.base import BaseCommand
-from django.shortcuts import get_object_or_404
-
+from django.core.management.base import BaseCommand, CommandError
 from reviews.models import (
-    Title, Category, Genre,
-    GenreTitle, Review, Comment, User
+    Title,
+    Category,
+    Genre,
+    GenreTitle,
+    User,
+    Review,
+    Comment,
 )
 
-class Command(BaseCommand):
-    help = 'Import data from a CSV file into the Title model'
 
+class Command(BaseCommand):
     def add_arguments(self, parser):
-        parser.add_argument('csv_file', type=str, help='The path to the CSV file')
+        parser.add_argument("csv_file", type=str)
+
+    def create_title(self, row):
+        category = Category.objects.get(id=row[3])
+        return Title(id=row[0], name=row[1], year=row[2], category=category)
+
+    def create_category(self, row):
+        return Category(id=row[0], name=row[1], slug=row[2])
+
+    def create_genre(self, row):
+        return Genre(id=row[0], name=row[1], slug=row[2])
+
+    def create_genre_title(self, row):
+        genre = Genre.objects.get(id=row[0])
+        title = Title.objects.get(id=row[1])
+        return GenreTitle(genre=genre, title=title)
+
+    def create_user(self, row):
+        return User(id=row[0], email=row[1], password=row[2])
+
+    def create_review(self, row):
+        user = User.objects.get(id=row[1])
+        title = Title.objects.get(id=row[2])
+        return Review(
+            id=row[0], user=user, title=title, text=row[3], score=row[4]
+        )
+
+    def create_comment(self, row):
+        user = User.objects.get(id=row[1])
+        review = Review.objects.get(id=row[2])
+        return Comment(id=row[0], user=user, review=review, text=row[3])
+
+    def import_data(self, reader, object_creator):
+        for row in reader:
+            try:
+                obj = object_creator(row)
+                obj.save()
+            except Exception as e:
+                self.stdout.write(
+                    self.style.ERROR("Невозможно импортировать. " + repr(e))
+                )
+                continue
 
     def handle(self, *args, **options):
-        csv_file = options['csv_file']
-        filename = os.path.basename(csv_file)
+        filename_to_creator_function = {
+            "titles.csv": self.create_title,
+            "category.csv": self.create_category,
+            "genre.csv": self.create_genre,
+            "genre_title.csv": self.create_genre_title,
+            "users.csv": self.create_user,
+            "review.csv": self.create_review,
+            "comments.csv": self.create_comment,
+        }
 
-        with open(csv_file, 'r', encoding='utf-8') as file:
-            reader = csv.reader(file)
-            next(reader)
-            for row in reader:
-                if filename == 'titles.csv':
-                    category_id = row[3]
-                    category = get_object_or_404(Category, id=category_id)
-                    try:
-                        object = Title(
-                            id=row[0],
-                            name=row[1],
-                            year=row[2],
-                            category=category
-                        )
-                        object.save()
-                    except Exception as e:
-                        print("Unable to upload file. "+repr(e))
-                if filename == 'category.csv':
-                    try:
-                        object = Category(
-                            id=row[0],
-                            name=row[1],
-                            slug=row[2],
-                        )
-                        object.save()
-                    except Exception as e:
-                        print("Unable to upload file. "+repr(e))
-                if filename == 'genre.csv':
-                    try:
-                        object = Genre(
-                            id=row[0],
-                            name=row[1],
-                            slug=row[2],
-                        )
-                        object.save()
-                    except Exception as e:
-                        print("Unable to upload file. "+repr(e))
-                if filename == 'genre_title.csv':
-                    try:
-                        object = GenreTitle(
-                            id=row[0],
-                            title_id=row[1],
-                            genre_id=row[2],
-                        )
-                        object.save()
-                    except Exception as e:
-                        print("Unable to upload file. "+repr(e))
-                if filename == 'users.csv':
-                    try:
-                        object = User(
-                            id=row[0],
-                            username=row[1],
-                            email=row[2],
-                            role=row[3],
-                            bio=row[4],
-                            first_name=row[5],
-                            last_name=[6]
-                        )
-                        object.save()
-                    except Exception as e:
-                        print("Unable to upload file. "+repr(e))
-                if filename == 'review.csv':
-                    user_id = row[3]
-                    author = get_object_or_404(User, id=user_id)
-                    try:
-                        object = Review(
-                            id=row[0],
-                            title_id=row[1],
-                            text=row[2],
-                            author=author,
-                            score=row[4],
-                            pub_date=row[5],
-                        )
-                        object.save()
-                    except Exception as e:
-                        print("Unable to upload file. "+repr(e))
-                if filename == 'comments.csv':
-                    user_id = row[3]
-                    author = get_object_or_404(User, id=user_id)
-                    review_id = row[1]
-                    review = get_object_or_404(Review, id=review_id)
-                    try:
-                        object = Comment(
-                            id=row[0],
-                            review=review,
-                            text=row[2],
-                            author=author,
-                            pub_date=row[4],
-                        )
-                        object.save()
-                    except Exception as e:
-                        print("Unable to upload file. "+repr(e))
-        self.stdout.write(self.style.SUCCESS('Data imported successfully'))
+        csv_file = options["csv_file"]
+
+        try:
+            with open(csv_file, "r", encoding="utf-8") as file:
+                reader = csv.reader(file)
+                next(reader)
+
+                try:
+                    filename = os.path.basename(csv_file)
+                    object_creator = filename_to_creator_function[filename]
+                except KeyError:
+                    raise CommandError(f'Не найден файл "{filename}"')
+
+                self.import_data(reader, object_creator)
+        except Exception as e:
+            raise CommandError(f"Невозможно открыть файл: {str(e)}")
+
+        self.stdout.write(self.style.SUCCESS("Дата успешно импортирована"))
